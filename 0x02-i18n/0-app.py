7722e38831f49c66e59444e77e47f65a719f6b74
collins-kimotho <collinskimotho16@gmail.com>
2024-11-05 12:14:29 +0300

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -16,19 +16,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
-Install the Babel Flask extension:
-
-$ pip3 install flask_babel==2.0.0
-Then instantiate the Babel object in your app. Store it in a module-level variable named babel.
-
-In order to configure available languages in our app, you will create a Config class that has a LANGUAGES class attribute equal to ["en", "fr"].
-
-Use Config to set Babel’s default locale ("en") and timezone ("UTC").
-
-Use that class as config for your Flask app.
-
-Repo:
-
-GitHub repository: alx-backend
-Directory: 0x02-i18n
-File: 1-app.py, templates/1-index.html
