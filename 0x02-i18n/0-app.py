0f9269b15b6db6b00606a2c541420a3b0f50bf1a
collins-kimotho <collinskimotho16@gmail.com>
2024-11-05 12:13:58 +0300

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+"""
+A basic Flask application with a single route.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    """
+    Renders the home page with a welcome message.
+    """
     return render_template('0-index.html')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
